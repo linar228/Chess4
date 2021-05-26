43d7d1b61890de8f04a7c3731cd4b5208aab5e52
линар валиев <linar369@mail.ru>
2021-05-26 13:16:36 +0300

    the board is almost finished

--- WpfChess/MainWindow.xaml.cs
+++ WpfChess/MainWindow.xaml.cs
@@ -23,6 +23,17 @@ namespace WpfChess
         public MainWindow()
         {
             InitializeComponent();
+
+            chessBoard.Rows = 8;
+            chessBoard.Columns = 8;
+
+            for(int i = 0; i< 32; i++)
+            {
+                Rectangle r1 = new Rectangle();
+                r1.Fill = Brushes.Black;
+                chessBoard.Children.Add(r1);
+            }
+
         }
     }
 }
